42f48931b790376606a5be0d83a5f0f8bb64702e
Andrea Crispo <a.crispo@fastcode.it>
2018-10-05 12:39:12 +0200

    init backend

---
+++ backend/src/main/java/my/app/bt/bettertogether/BetterTogetherApplication.java
@@ -0,0 +1,12 @@
+package my.app.bt.bettertogether;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetterTogetherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetterTogetherApplication.class, args);
+	}
+}
