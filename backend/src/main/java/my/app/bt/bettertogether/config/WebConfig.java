a81e84b318baef2b6554da0af7fd94aec4b54b2d
Andrea Crispo <andrea.crispo.cr@gmail.com>
2018-10-22 19:04:21 +0200

    Backend and frontend integration

--- backend/src/main/java/my/app/bt/bettertogether/config/WebConfig.java
+++ backend/src/main/java/my/app/bt/bettertogether/config/WebConfig.java
@@ -1,13 +1,31 @@
 package my.app.bt.bettertogether.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
 import org.springframework.web.reactive.config.CorsRegistry;
 import org.springframework.web.reactive.config.EnableWebFlux;
+import org.springframework.web.reactive.config.ResourceHandlerRegistry;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
+import org.springframework.web.reactive.resource.PathResourceResolver;
+import reactor.core.publisher.Mono;
+
+import java.io.IOException;
+
 
 @Configuration
 @EnableWebFlux
-public class CORSFilter implements WebFluxConfigurer {
+public class WebConfig implements WebFluxConfigurer {
+
+    private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
+            "classpath:/META-INF/resources/", "classpath:/resources/",
+            "classpath:/static/", "classpath:/public/" };
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
+    }
+
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
@@ -25,3 +43,5 @@ public class CORSFilter implements WebFluxConfigurer {
                 ).maxAge(3600);
     }
 }
+
+
--- backend/src/main/java/my/app/bt/bettertogether/config/WebSecurityConfig.java
+++ backend/src/main/java/my/app/bt/bettertogether/config/WebSecurityConfig.java
@@ -2,13 +2,17 @@ package my.app.bt.bettertogether.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
+@Configuration
 @EnableWebFluxSecurity
 @EnableReactiveMethodSecurity
 public class WebSecurityConfig {
@@ -22,25 +26,31 @@ public class WebSecurityConfig {
         this.securityContextRepository = securityContextRepository;
     }
 
+
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
         return http
                 .csrf().disable()
-                .formLogin().disable()
                 .httpBasic().disable()
+                .formLogin().disable()
+                .logout().disable()
+
                 .authenticationManager(authenticationManager)
                 .securityContextRepository(securityContextRepository)
                 .authorizeExchange()
+
                 // Permit cors request
                 .pathMatchers(org.springframework.http.HttpMethod.OPTIONS,"/**").permitAll()
-                //.pathMatchers(String.valueOf(HttpMethod.OPTIONS)).permitAll()
                 // Permint onyl auth endpoint (login and signup)
-                .pathMatchers("/auth/**").permitAll()
-                // everelse onder authentication
-                .anyExchange().authenticated()
+             //   .pathMatchers("/auth/**").permitAll()
+                // everything else under authentication
+              //  .anyExchange().authenticated()
+                .pathMatchers("/**").permitAll()
+                .pathMatchers("/api/**").authenticated()
                 .and().build();
 
     }
+
 
     @Bean
     public PasswordEncoder passwordEncoder() {
