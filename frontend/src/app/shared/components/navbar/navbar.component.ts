7befb77a75c3f23f1fffcb5f0da6cfa3ba6a0a2d
Andrea Crispo <andrea.crispo.cr@gmail.com>
2018-11-11 16:14:10 +0100

    Fix responsive layout

--- frontend/src/app/shared/components/navbar/navbar.component.ts
+++ frontend/src/app/shared/components/navbar/navbar.component.ts
@@ -9,6 +9,8 @@ import { Router } from '@angular/router';
 })
 export class NavbarComponent implements OnInit {
 
+  navbarOpen: boolean = false;
+  
   constructor(private authService: AuthService,
               private router: Router) { }
 
@@ -22,6 +24,11 @@ export class NavbarComponent implements OnInit {
   logout() {
     this.authService.logout();
     this.router.navigate(['/login']);
+  }
+
+
+  toggleNavbar() {
+    this.navbarOpen = !this.navbarOpen;
   }
 
 }
